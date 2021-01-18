ecf1122b7c62530d03ed54e4de482a56f328920e
deyandyankov1 <dveskov@gmail.com>
2021-01-18 20:23:38 +0100

    Initial commit

---
+++ 03. Leonardo DiCaprio Oscars.py
@@ -0,0 +1,10 @@
+leo = int(input())
+
+if leo == 86:
+    print("Not even for Wolf of Wall Street?!")
+elif leo > 88:
+    print("Leo got one already!")
+elif  leo >= 88:
+    print("Leo finally won the Oscar! Leo is happy")
+else:
+    print("When will you give Leo an Oscar?")
